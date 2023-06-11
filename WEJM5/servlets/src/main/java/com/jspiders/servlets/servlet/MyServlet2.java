a97d625616f6f04dd52f7dbf56571c003c0dd330
Bankarashu <68050769+Bankarashu@users.noreply.github.com>
2023-06-11 17:04:12 +0530

    Add files via upload

---
+++ WEJM5/servlets/src/main/java/com/jspiders/servlets/servlet/MyServlet2.java
@@ -0,0 +1,27 @@
+package com.jspiders.servlets.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet2 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>Myservlet2</h1>");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+}
---
+++ WEJM5/servlets/src/main/java/com/jspiders/servlets/servlet/NetflixServlet.java
@@ -0,0 +1,26 @@
+package com.jspiders.servlets.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/netflix")
+public class NetflixServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+			String uri = request.getRequestURI();
+			writer.println(uri);
+		
+		
+	}
+
+}
